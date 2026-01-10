7cd7a7e53d5437f705885d8481f839d8eb91a503
mihaelcacic <mc55778@fer.hr>
2026-01-10 20:50:29 +0100

    dodano sve

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+
+export default function App() {
+  
+  return (
+      <>
+      <Header />
+      <Footer />
+      </>
+  );
+}
+
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer () {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} FinTrack. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer
