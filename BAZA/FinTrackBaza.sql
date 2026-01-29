4fca0351f77d38afed0c7f8828208a437b1c4a15
Antonio Valec <av56151@fer.hr>
2026-01-29 19:35:58 +0100

    Update FinTrackBaza.sql

--- BAZA/FinTrackBaza.sql
+++ BAZA/FinTrackBaza.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
                        password_hash TEXT NOT NULL,
                        username VARCHAR(100),
                        created_at TIMESTAMP DEFAULT NOW(),
-                        isAdmin BOOLEAN DEFAULT false
+                       is_Admin BOOLEAN DEFAULT false
 );
 
 CREATE TABLE categories (
